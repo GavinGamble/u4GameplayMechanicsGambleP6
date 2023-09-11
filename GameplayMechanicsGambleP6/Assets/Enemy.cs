a948bd3b0e6b1a0b04860ba63fc0e2f342a3e899
GavinGamble <710145@student.dusd.net>
2023-09-11 13:50:51 -0700

    9/11/23

--- GameplayMechanicsGambleP6/Assets/Enemy.cs
+++ GameplayMechanicsGambleP6/Assets/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float speed = 2.0f;
+    public float speed = 5.0f;
     private Rigidbody enemyRb;
     private GameObject player;
     // Start is called before the first frame update
@@ -17,6 +17,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
     }
 }
