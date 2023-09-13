cea028834b8abeea02e2b8019c098efd160ac005
GavinGamble <710145@student.dusd.net>
2023-09-13 13:49:13 -0700

    9/13/23

--- GameplayMechanicsGambleP6/Assets/Enemy.cs
+++ GameplayMechanicsGambleP6/Assets/Enemy.cs
@@ -21,4 +21,5 @@ public class Enemy : MonoBehaviour
 
         enemyRb.AddForce(lookDirection * speed);
     }
+    
 }
--- GameplayMechanicsGambleP6/Assets/PlayerController.cs
+++ GameplayMechanicsGambleP6/Assets/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerUp;
+    public GameObject powerUpIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,34 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerUpIndicator.transform.position = transfrom.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerUpIndicator.gameObject.SetActive(true);
+        }
+        IEnumerator PowerupCountdownRoutine()
+        {
+            yield return new WaitForSeconds(7);
+            hasPowerUp = false;
+            powerUpIndicator.gameObject.SetActive(false);
+        }        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerUp);
+        }
     }
 }
